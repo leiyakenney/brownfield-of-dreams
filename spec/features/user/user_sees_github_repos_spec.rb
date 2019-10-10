f920cba763f8a1ca6ba9b27666911f06d1e4f51a
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-10 09:36:49 -0600

    Adds tests for followers showing on a dashboard if GitHub profile is linked.
    Co-authored-by: Joshua Sherwood 
    <49769068+joshsherwood1@users.noreply.github.com>

--- spec/features/user/user_sees_github_repos_spec.rb
+++ spec/features/user/user_sees_github_repos_spec.rb
@@ -17,9 +17,27 @@ describe 'A registered user' do
       expect(page).to have_css(".repo_name_and_link")
     end
   end
+
+  it 'I can see list of all followers with their handles linking to their Github profile', :vcr do
+    json_response = File.open("./fixtures/github_followers_data.json")
+    stub_request(:get, "https://api.github.com/user/followers").to_return(status: 200, body: json_response)
+
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/dashboard"
+
+    expect(page).to have_content("Followers")
+
+    within(".followers") do
+      expect(page).to have_css(".username_and_link")
+    end
+  end
 end
 
 # As a logged in user
 # When I visit /dashboard
 # Then I should see a section for "Github"
-# And under that section I should see a list of 5 repositories with the name of each Repo linking to the repo on Github
+# And under that section I should see another section titled "Followers"
+# And I should see list of all followers with their handles linking to their Github profile
