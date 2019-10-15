3b6925dcea9e8919a667ca05135e0889515a70cc
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-15 15:29:22 -0600

    Delete unnecessary notification controller
    Co-authored-by: Leiya Kenney 
    <45922590+leiyakenney@users.noreply.github.com>
    
    Co-Authored-By: Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>

--- app/controllers/notification_controller.rb
+++
@@ -1,7 +0,0 @@
-class NotificationController < ApplicationController
-  def create
-    EmailActivatorMailer.inform(current_user, params[:email]).deliver_now
-    flash[:notice] = "Activation email sent."
-    redirect_to root_path
-  end
-end
