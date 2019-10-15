e25b9308ba6fb24e9996008c128bbe595b9619fc
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-15 15:29:22 -0600

    Create feature test for user registering and getting activation email to activate account and test for activator email
    Co-authored-by: Leiya Kenney 
    <45922590+leiyakenney@users.noreply.github.com>
    
    Co-Authored-By: Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>

--- spec/features/user/user_can_register_their_account_spec.rb
+++ spec/features/user/user_can_register_their_account_spec.rb
@@ -2,6 +2,33 @@ require 'rails_helper'
 
 describe 'A Non-Registered User' do
   it "sees an activation message in their email that directs them to a confirmation page" do
+    email = 'jimbob@aol.com'
+    first_name = 'Jim'
+    last_name = 'Bob'
+    password = 'password'
+    password_confirmation = 'password'
+
+    visit '/'
+
+    click_on 'Sign In'
+
+    expect(current_path).to eq(login_path)
+
+    click_on 'Sign up now.'
+
+    expect(current_path).to eq(new_user_path)
+
+    fill_in 'user[email]', with: email
+    fill_in 'user[first_name]', with: first_name
+    fill_in 'user[last_name]', with: last_name
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: password
+
+    click_on'Create Account'
+
+    
+
+
     # As a non-activated user
     # When I check my email for the registration email
     # I should see a message that says "Visit here to activate your account."
@@ -16,3 +43,4 @@ describe 'A Non-Registered User' do
     # Then I should see "Status: Active"
 
   end
+end
