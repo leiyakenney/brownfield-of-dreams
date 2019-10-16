8da5ead3ea82affc9ad046cb0df4423d815ff1eb
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-16 15:50:48 -0600

    Creates Invite Mailer to add functionality for sending an invite email to a friend
    Co-authored-by: Joshua Sherwood 
    <49769068+joshsherwood1@users.noreply.github.com>
    
    Co-Authored-By: null <jkjgsherwood@gmail.com>

---
+++ app/mailers/invite_mailer.rb
@@ -0,0 +1,10 @@
+class InviteMailer < ApplicationMailer
+  def invite(user_id, friend_email, friend_name)
+    @user = User.find(user_id)
+    @friend_email = friend_email
+    @friend_name = friend_name
+    # binding.pry
+    mail(to: @friend_email, subject: "Your friend has invited you to join Brownfield of Dreams!")
+  end
+
+end
