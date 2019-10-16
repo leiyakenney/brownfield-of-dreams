24d25030166c933e88920a48e80299d2ad5fa0f2
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-16 15:50:48 -0600

    Adds email method to github_search_results service so we can find a person's info through their github username
    Co-authored-by: Joshua Sherwood 
    <49769068+joshsherwood1@users.noreply.github.com>
    
    Co-Authored-By: null <jkjgsherwood@gmail.com>

--- app/services/github_search_results.rb
+++ app/services/github_search_results.rb
@@ -21,6 +21,11 @@ class GithubSearchResults
     followed_accounts_data = JSON.parse(response.body, symbolize_names: true)
   end
 
+  def email(github_username)
+    response = conn.get("/users/#{github_username}")
+    email_data = JSON.parse(response.body, symbolize_names: true)
+  end
+
   def conn
     Faraday.new(url: 'https://api.github.com') do |faraday|
       faraday.adapter Faraday.default_adapter
