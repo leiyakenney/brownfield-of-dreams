56ca0b6b05305158427ed1e75a4d4b5194336d1e
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-16 21:54:39 -0600

    Change cassettes

--- spec/features/user/user_can_send_invite_spec.rb
+++ spec/features/user/user_can_send_invite_spec.rb
@@ -5,7 +5,7 @@ describe "A registered user" do
     json_response = File.open('./fixtures/github_invite_data_with_email.json')
     stub_request(:get, 'https://api.github.com/users/leiyakenney').to_return(status: 200, body: json_response)
 
-    user = create(:user, github_token: ENV["LEIYA_GITHUB_API_KEY"], github_username: 45922590, first_name: "Leiya", last_name: "Kenney")
+    user = create(:user, github_token: ENV["GITHUB_API_KEY"], github_username: 49769068, first_name: "Leiya", last_name: "Kenney")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit dashboard_path
@@ -24,7 +24,7 @@ describe "A registered user" do
     json_response = File.open('./fixtures/github_invite_data_no_email.json')
     stub_request(:get, 'https://api.github.com/users/joshsherwood1').to_return(status: 200, body: json_response)
 
-    user = create(:user, github_token: ENV["LEIYA_GITHUB_API_KEY"], github_username: 45922590, first_name: "Leiya", last_name: "Kenney")
+    user = create(:user, github_token: ENV["GITHUB_API_KEY"], github_username: 49769068, first_name: "Leiya", last_name: "Kenney")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit dashboard_path
