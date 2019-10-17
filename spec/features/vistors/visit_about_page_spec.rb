68ca71947c86571e8f0638fe55dfdabd942b1847
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-17 00:58:19 -0600

    Create feature spec tests for about and get started pages

---
+++ spec/features/vistors/visit_about_page_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'visitor' do
+  it 'visits about page' do
+    visit "/about"
+    expect(page).to have_content("This application is designed to pull in youtube information to populate tutorials from Turing School of Software and Design's youtube channel")
+  end
+end
---
+++ spec/features/vistors/visit_get_started_page_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'visitor' do
+  it 'visits gets started page' do
+    visit "/get_started"
+    expect(page).to have_content("Filter results by selecting a filter on the side bar of the")
+  end
+end
