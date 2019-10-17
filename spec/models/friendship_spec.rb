8b217da1044a386d5db56a3b234732d232febfbb
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-17 00:57:56 -0600

    Add model spec tests for followed account, follower, friendship, and repo
    Co-Authored-By: Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>

---
+++ spec/models/friendship_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Friendship, type: :model do
+  describe 'relationships' do
+    it { should belong_to(:user) }
+    it { should belong_to(:friend) }
+  end
+end
